e6b2320e1be1a86a0fe92f38282191d51b9e09f7
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2017-02-20 08:37:13 +0100

    Add fontawesome

--- modules/Home.js
+++ modules/Home.js
@@ -4,7 +4,18 @@ import input from '../public/home.md'
 export default React.createClass({
   render() {
     return (
-      <div dangerouslySetInnerHTML={ {__html: input} } />
+      <div>
+        <div dangerouslySetInnerHTML={ {__html: input} } />
+        <i className="fa fa-quora"></i>
+        <i className="fa fa-medium"></i>
+        <i className="fa fa-facebook"></i>
+        <i className="fa fa-xing"></i>
+        <i className="fa fa-linkedin"></i>
+        <i className="fa fa-bitbucket"></i>
+        <i className="fa fa-github"></i>
+        <i className="fa fa-skype"></i>
+        <i className="fa fa-spotify"></i>
+      </div>
     );
   }
 });
